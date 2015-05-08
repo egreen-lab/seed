95c1c9b1624ce419bb1ab982160429def9350c6a
dewmal <dewmalnilanka@gmail.com>
2015-05-08 05:36:35 +0530

    Create Gradle Task to auto build and copy to fil deploye dir

---
+++ kernel/src/main/java/org/egreen/seed/kernel/SeedActivator.java
@@ -0,0 +1,19 @@
+package org.egreen.seed.kernel;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by dewmal on 5/6/15.
+ */
+public class SeedActivator implements BundleActivator {
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("Working App TEst78021");
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
